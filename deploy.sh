8d5dbdec49605821147bc1aa0d1fc8a52ba60332
Andy Nygard <andreas.nygard@gmail.com>
2015-06-14 21:06:10 +1000

    Added web server for static files.

--- deploy.sh
+++ deploy.sh
@@ -12,3 +12,6 @@ set -e
 
 echo Starting reverse proxy...
 docker run -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock jwilder/nginx-proxy
+
+echo Starting static site...
+docker run -d -p 8080:80 -e VIRTUAL_HOST=128.199.78.177 scurvy/site-static
